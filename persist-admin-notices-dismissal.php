76cd719818527f53190ae2df928b51af8bad244e
Andy Fragen <andy@thefragens.com>
2016-07-30 16:19:20 -0700

    add to class for ease management
    Allows for easier checking against use in multiple plugins. Should help to avoid fatals.
    
    move to yoda conditionals and WP Coding Guidelines for spacing

--- persist-admin-notices-dismissal.php
+++ persist-admin-notices-dismissal.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Persist Admin notices Dismissal
  *
@@ -18,67 +19,79 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package Persist Admin notices Dismissal
- * @author Agbonghama Collins
+ * @author  Agbonghama Collins
  * @license http://www.gnu.org/licenses GNU General Public License
  */
 
+if ( ! class_exists( 'PAnD' ) ) {
 
-add_action('admin_enqueue_scripts', function () {
-    wp_enqueue_script('dismissible-notices',
-        plugin_dir_url(__FILE__) . 'dismiss-notice.js',
-        false,
-        true
-    );
+	class PAnD {
 
-    wp_localize_script('dismissible-notices', 'dismissible_notice',
-        array(
-            'nonce' => wp_create_nonce('dismissible-notice'),
-        )
-    );
-});
+		public function __construct() {
 
+			/**
+			 * Enqueue javascript and register tags.
+			 */
+			add_action( 'admin_enqueue_scripts', function () {
+				wp_enqueue_script( 'dismissible-notices',
+					plugin_dir_url( __FILE__ ) . 'dismiss-notice.js',
 					array( 'jquery', 'common' ),
+					false,
+					true
+				);
 
-/**
- * Handles Ajax request to persist notices dismissal.
- */
-add_action('wp_ajax_dismiss_admin_notice', function () {
-    $option_name        = sanitize_text_field($_POST['option_name']);
-    $dismissible_length = sanitize_text_field($_POST['dismissible_length']);
-
-    if ($dismissible_length != 'forever') {
-        $dismissible_length = time() + strtotime(absint($dismissible_length) . 'days');
-    }
-
-    if (wp_verify_nonce($_REQUEST['nonce'], 'pp-dismissible-notice') && strpos($option_name, 'data-') !== false) {
-        add_option($option_name, $dismissible_length);
-    }
-
-    add_option($option_name, $dismissible_length);
-    wp_die();
-});
+				wp_localize_script( 'dismissible-notices', 'dismissible_notice',
+					array(
+						'nonce' => wp_create_nonce( 'dismissible-notice' ),
+					)
+				);
+			} );
 
 
-/**
- * Is admin notice active?
- *
- * @param string $arg
- *
- * @return bool
- */
-function is_admin_notice_active($arg)
-{
-    $array       = explode('-', $arg);
-    $length      = array_pop($array);
-    $option_name = implode('-', $array);
+			/**
+			 * Handles Ajax request to persist notices dismissal.
+			 */
+			add_action( 'wp_ajax_dismiss_admin_notice', function () {
+				$option_name        = sanitize_text_field( $_POST['option_name'] );
+				$dismissible_length = sanitize_text_field( $_POST['dismissible_length'] );
 
-    $db_record = get_option($option_name);
+				if ( 'forever' != $dismissible_length ) {
+					$dismissible_length = time() + strtotime( absint( $dismissible_length ) . 'days' );
+				}
 
-    if ($db_record == 'forever') {
-        return false;
-    } elseif (absint($db_record) >= time()) {
-        return false;
-    } else {
-        return true;
-    }
+				if ( wp_verify_nonce( $_REQUEST['nonce'], 'pp-dismissible-notice' ) && false !== strpos( $option_name, 'data-' ) ) {
+					add_option( $option_name, $dismissible_length );
+				}
+
+				add_option( $option_name, $dismissible_length );
+				wp_die();
+			} );
+		}
+
+		/**
+		 * Is admin notice active?
+		 *
+		 * @param string $arg
+		 *
+		 * @return bool
+		 */
+		public function is_admin_notice_active( $arg ) {
+			$array       = explode( '-', $arg );
+			$length      = array_pop( $array );
+			$option_name = implode( '-', $array );
+
+			$db_record = get_option( $option_name );
+
+			if ( 'forever' == $db_record ) {
+				return false;
+			} elseif ( absint( $db_record ) >= time() ) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+	}
+
+	new PAnD();
 }
