0a59c9dc4ce1adc119981435f43438ec030f01bc
dangoodman <dangoodman@users.noreply.github.com>
2017-06-05 18:21:57 +0300

    No reason to check if class_exists
    It seems there is no reason to check if class is already loaded. Composer autoloader and require_once() do the job better.

--- persist-admin-notices-dismissal.php
+++ persist-admin-notices-dismissal.php
@@ -25,85 +25,81 @@
  * @version 1.3
  */
 
-if ( ! class_exists( 'PAnD' ) ) {
+/**
+ * Class PAnD
+ */
+class PAnD {
 
 	/**
-	 * Class PAnD
+	 * Init hooks.
 	 */
-	class PAnD {
+	public static function init() {
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'load_script' ) );
+		add_action( 'wp_ajax_dismiss_admin_notice', array( __CLASS__, 'dismiss_admin_notice' ) );
+	}
 
-		/**
-		 * Init hooks.
-		 */
-		public static function init() {
-			add_action( 'admin_enqueue_scripts', array( __CLASS__, 'load_script' ) );
-			add_action( 'wp_ajax_dismiss_admin_notice', array( __CLASS__, 'dismiss_admin_notice' ) );
+	/**
+	 * Enqueue javascript and variables.
+	 */
+	public static function load_script() {
+		wp_enqueue_script(
+			'dismissible-notices',
+			plugins_url( 'dismiss-notice.js', __FILE__ ),
+			array( 'jquery', 'common' ),
+			false,
+			true
+		);
+
+		wp_localize_script(
+			'dismissible-notices',
+			'dismissible_notice',
+			array(
+				'nonce' => wp_create_nonce( 'dismissible-notice' ),
+			)
+		);
+	}
+
+	/**
+	 * Handles Ajax request to persist notices dismissal.
+	 * Uses check_ajax_referer to verify nonce.
+	 */
+	public static function dismiss_admin_notice() {
+		$option_name        = sanitize_text_field( $_POST['option_name'] );
+		$dismissible_length = sanitize_text_field( $_POST['dismissible_length'] );
+		$transient          = 0;
+
+		if ( 'forever' != $dismissible_length ) {
+			// If $dismissible_length is not an integer default to 1
+			$dismissible_length = ( 0 == absint( $dismissible_length ) ) ? 1 : $dismissible_length;
+			$transient          = absint( $dismissible_length ) * DAY_IN_SECONDS;
+			$dismissible_length = strtotime( absint( $dismissible_length ) . ' days' );
 		}
 
-		/**
-		 * Enqueue javascript and variables.
-		 */
-		public static function load_script() {
-			wp_enqueue_script(
-				'dismissible-notices',
-				plugins_url( 'dismiss-notice.js', __FILE__ ),
-				array( 'jquery', 'common' ),
-				false,
-				true
-			);
+		check_ajax_referer( 'dismissible-notice', 'nonce' );
+		set_site_transient( $option_name, $dismissible_length, $transient );
+		wp_die();
+	}
 
-			wp_localize_script(
-				'dismissible-notices',
-				'dismissible_notice',
-				array(
-					'nonce' => wp_create_nonce( 'dismissible-notice' ),
-				)
-			);
+	/**
+	 * Is admin notice active?
+	 *
+	 * @param string $arg data-dismissible content of notice.
+	 *
+	 * @return bool
+	 */
+	public static function is_admin_notice_active( $arg ) {
+		$array       = explode( '-', $arg );
+		$length      = array_pop( $array );
+		$option_name = implode( '-', $array );
+		$db_record   = get_site_transient( $option_name );
+
+		if ( 'forever' == $db_record ) {
+			return false;
+		} elseif ( absint( $db_record ) >= time() ) {
+			return false;
+		} else {
+			return true;
 		}
-
-		/**
-		 * Handles Ajax request to persist notices dismissal.
-		 * Uses check_ajax_referer to verify nonce.
-		 */
-		public static function dismiss_admin_notice() {
-			$option_name        = sanitize_text_field( $_POST['option_name'] );
-			$dismissible_length = sanitize_text_field( $_POST['dismissible_length'] );
-			$transient          = 0;
-
-			if ( 'forever' != $dismissible_length ) {
-				// If $dismissible_length is not an integer default to 1
-				$dismissible_length = ( 0 == absint( $dismissible_length ) ) ? 1 : $dismissible_length;
-				$transient          = absint( $dismissible_length ) * DAY_IN_SECONDS;
-				$dismissible_length = strtotime( absint( $dismissible_length ) . ' days' );
-			}
-
-			check_ajax_referer( 'dismissible-notice', 'nonce' );
-			set_site_transient( $option_name, $dismissible_length, $transient );
-			wp_die();
-		}
-
-		/**
-		 * Is admin notice active?
-		 *
-		 * @param string $arg data-dismissible content of notice.
-		 *
-		 * @return bool
-		 */
-		public static function is_admin_notice_active( $arg ) {
-			$array       = explode( '-', $arg );
-			$length      = array_pop( $array );
-			$option_name = implode( '-', $array );
-			$db_record   = get_site_transient( $option_name );
-
-			if ( 'forever' == $db_record ) {
-				return false;
-			} elseif ( absint( $db_record ) >= time() ) {
-				return false;
-			} else {
-				return true;
-			}
-		}
-
 	}
 
 }
