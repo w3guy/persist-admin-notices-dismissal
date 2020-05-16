1cbfa24cd0cd17c3902b455d2c03e5aed0475086
Lars Koudal <lars@koudal.dk>
2020-05-16 17:19:27 +0200

    Update dismiss-notice.js

--- dismiss-notice.js
+++ dismiss-notice.js
@@ -2,7 +2,7 @@
 	//shorthand for ready event.
 	$(
 		function () {
-			$( 'div[data-dismissible] button.notice-dismiss, div[data-dismissible] notice-dismiss' ).click(
+			$( 'div[data-dismissible] button.notice-dismiss, div[data-dismissible] .notice-dismiss' ).click(
 				function (event) {
 					event.preventDefault();
 					var $this = $( this );
